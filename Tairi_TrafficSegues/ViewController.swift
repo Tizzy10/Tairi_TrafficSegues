978aa0c55e999df53ce2fef8badbb0bf374da06a
TizzJohnson <tizznasty10@gmail.com>
2021-04-01 11:10:22 -0400

    Initial Commit

---
+++ Tairi_TrafficSegues/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tairi_TrafficSegues
+//
+//  Created by Tairi on 4/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
